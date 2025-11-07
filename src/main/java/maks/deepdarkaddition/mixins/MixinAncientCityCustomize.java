ddd7c6a605f6d0919dfa93872874573c2b7210fa
Макс Носов <nmaks6381@gmail.com>
2025-11-07 17:27:47 +0300

    updated files(mixins not work)

--- src/main/java/maks/deepdarkaddition/mixins/MixinAncientCityCustomize.java
+++ src/main/java/maks/deepdarkaddition/mixins/MixinAncientCityCustomize.java
@@ -1,14 +1,16 @@
 package maks.deepdarkaddition.mixins;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.monster.warden.Warden;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(Warden.class)
 public abstract class MixinAncientCityCustomize {
-    @Inject(method = "tick", at = @At("TAIL"))
-    private void onTick() {
+    @Inject(method = "canRide", at = @At("TAIL"))
+    protected void onCanRide(Entity pVehicle) {
         System.out.println("dda_succesfull");
     }
 }
