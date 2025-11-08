1e3ac8d5c5fa61aad75585bdeed21dbe64a23749
Макс Носов <nmaks6381@gmail.com>
2025-11-08 19:03:51 +0300

    updated files

--- src/main/java/maks/deepdarkaddition/mixins/MixinSculkCatalystBlockEntity.java
+++ src/main/java/maks/deepdarkaddition/mixins/MixinSculkCatalystBlockEntity.java
@@ -9,12 +9,9 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.SculkSpreader;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.SculkCatalystBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.gameevent.GameEventListener;
 import net.minecraft.world.level.gameevent.PositionSource;
@@ -23,42 +20,23 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import javax.annotation.Nullable;
-
-@Mixin(SculkCatalystBlockEntity.class)
-public class MixinSculkCatalystBlockEntity extends BaseEntityBlock implements GameEventListener.Holder<SculkCatalystBlockEntity.CatalystListener> {
-    @Shadow
-    private SculkCatalystBlockEntity.CatalystListener catalystListener;
+@Mixin(SculkCatalystBlockEntity.CatalystListener.class)
+public abstract class MixinSculkCatalystBlockEntity implements GameEventListener {
 
     @Shadow
-    public static BooleanProperty PULSE;
+    private void tryAwardItSpreadsAdvancement(Level pLevel, LivingEntity pEntity) {}
 
     @Shadow
     SculkSpreader sculkSpreader;
 
     @Shadow
-    private void tryAwardItSpreadsAdvancement(Level pLevel, LivingEntity pEntity) {
-    }
+    private BlockState blockState;
 
     @Shadow
     private PositionSource positionSource;
 
     @Shadow
-    private BlockState blockState;
-
-    @Shadow
-    private void bloom(ServerLevel pLevel, BlockPos pPos, BlockState pState, RandomSource pRandom) {
-    }
-
-    public MixinSculkCatalystBlockEntity(Properties p_222090_) {
-        super(p_222090_);
-        this.registerDefaultState((BlockState) ((BlockState) this.stateDefinition.any()).setValue(PULSE, false));
-    }
-
-    @Nullable
-    public BlockEntity newBlockEntity(BlockPos p_222117_, BlockState p_222118_) {
-        return new SculkCatalystBlockEntity(p_222117_, p_222118_);
-    }
+    private void bloom(ServerLevel pLevel, BlockPos pPos, BlockState pState, RandomSource pRandom) {}
 
     @Overwrite
     public boolean handleGameEvent(ServerLevel pLevel, GameEvent pGameEvent, GameEvent.Context pContext, Vec3 pPos) {
@@ -88,11 +66,5 @@ public class MixinSculkCatalystBlockEntity extends BaseEntityBlock implements Ga
         }
 
         return false;
-    }
-
-
-    @Override
-    public SculkCatalystBlockEntity.CatalystListener getListener() {
-        return this.catalystListener;
     }
 }
